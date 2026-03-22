c54cc7f568d60b221a831e449061a36f20f39d30
UBONGO2000 <gadyubongo@gmail.com>
2026-03-22 13:38:33 +0100

    amelioration des configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  output:'static',
   image: {
     service: { entrypoint: 'astro/assets/services/sharp' },
   },
